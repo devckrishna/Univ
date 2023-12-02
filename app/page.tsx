12fa949042ae212aab7dcace7bdf8f78d6085f42
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2023-12-03 01:25:18 +0530

    vercel and prisma integreation

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,10 @@
-export default function Home() {
+import { db } from "@/utils/db";
+
+const Home = async () => {
+  const user = await db.user.findMany();
+  console.log(user);
+
   return <div>Hello World</div>;
-}
+};
+
+export default Home;
