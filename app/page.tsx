d2df1b7e8092c5a892d43379342caf75db72d34a
Dev C. Krishna <dev.c.krishna.123@gmail.com>
2023-12-03 16:02:08 +0530

    removed old code

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,8 @@
 import { db } from "@/utils/db";
 
 const Home = async () => {
-  const user = await db.user.findMany();
-  console.log(user);
+  // const user = await db.user.findMany();
+  // console.log(user);
 
   return <main className="h-full w-full"></main>;
 };
