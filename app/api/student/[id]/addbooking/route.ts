23d1ffa91146768a60a7742f9e2ff35b4e14fcf7
Chirag Jindal <69391451+Chirag77302@users.noreply.github.com>
2023-12-09 00:25:50 +0530

    refactored backend routes

--- app/api/student/[id]/addbooking/route.ts
+++ app/api/student/[id]/addbooking/route.ts
@@ -1,5 +1,8 @@
 import { db } from "@/utils/db";
 import { NextResponse,NextRequest } from "next/server";
+import { PrismaClient } from "@prisma/client";
+
+const prisma = new PrismaClient();
 
 // export async function GET(req: Request) {
 //   try {
